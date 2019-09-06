c7314728569a7efe054b27deef2367d681426495
shaojwa <shaojwa@gmail.com>
2019-09-06 13:55:39 +0800

    Update bug_operation_record.sh

--- bug_operation_record.sh
+++ bug_operation_record.sh
@@ -8,3 +8,11 @@
 
      /etc/profile.d/set_operation.sh
      
+
+#### 记录的时间
+
+shell下正常运行一条命令，这条命令记录到operation的时间也是这条命令执行完的时间。可以通过以下脚本测试：
+
+     date; sleep 120; date
+
+等这条命令执行完之后才会记录到operation中。
